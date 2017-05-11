6ee3f32a66196106eabf12aa0d2773bdbc0f706a
abhanneman10 <alexbhanneman@local-admins-MacBook-Pro-5.local>
2017-05-10 22:52:07 -0500

    udpates

--- src/app/about/about.controller.js
+++ src/app/about/about.controller.js
@@ -9,5 +9,98 @@
   function AboutController() {
     var vm = this;
 
+    vm.skills = [
+      {
+        type: '.NET',
+        current: true
+      },
+      {
+        type: 'C#',
+        current: true
+      },
+      {
+        type: 'ElasticSearch',
+        current: true
+      },
+      {
+        type: 'Java',
+        current: false
+      },
+      {
+        type: 'VisualBasic',
+        current: true
+      },
+      {
+        type: 'WPF',
+        current: true
+      },
+      {
+        type: 'SQL Server',
+        current: true
+      },
+      {
+        type: 'PHP',
+        current: false
+      },
+      {
+        type: 'Redis Cache',
+        current: true
+      },
+      {
+        type: 'REST web services',
+        current: true
+      },
+      {
+        type: 'MS Azure\'s App Insights',
+        current: true
+      }
+    ];
+
+    vm.skillsWeb = [
+      {
+        type: 'AngularJS',
+        current: true
+      },
+      {
+        type: 'NodeJs',
+        current: true
+      },
+      {
+        type: 'JQuery',
+        current: true
+      },
+      {
+        type: 'JavaScript',
+        current: true
+      },
+      {
+        type: 'HTML',
+        current: true
+      },
+      {
+        type: 'CSS',
+        current: true
+      },
+      {
+        type: 'Boostrap',
+        current: true
+      },
+      {
+        type: 'React',
+        current: false
+      },
+      {
+        type: 'Redux',
+        current: false
+      },
+      {
+        type: 'Material UI',
+        current: false
+      },
+      {
+        type: 'Auth0',
+        current: true
+      }
+    ]
   }
 })();
--- src/app/portfolio/portfolio.controller.js
+++ src/app/portfolio/portfolio.controller.js
@@ -9,5 +9,30 @@
   function PortfolioController() {
     var vm = this;
 
+    vm.items = [
+      {
+        title: 'Bluffs Bar Website',
+        link: 'http://www.thebluffsbar.com',
+        image: '../../assets/images/bluffsbar.png',
+        description: 'MEAN Stack (MongoDB, Express, AngularJs, NodeJs) website styled using Bootstrap.' +
+        ' My senior year of college (\'14-\'15) I taught myself M.E.A.N. and built this website with two other colleagues as an extracurricular project.' +
+        ' We built this website from scratch for The Bluffs Bar, located in Hager City, WI.'
+      },
+      {
+        title: 'Dependable Builders Website',
+        link: 'http://www.dependablebuilders.com',
+        image: '../../assets/images/dependablebuilders.png',
+        description: 'The summer after my senior year of college I enhanced this currently existing site - Dependable Builders, located in Lakeville, MN.' +
+        ' I implemented a content management system using drag and drop for images to add and delete pictures, and enhanced ' +
+        ' the home, services, photos, and testimonials pages.'
+      },
+      {
+        title: 'My Old Resume Website',
+        link: 'http://alexbhanneman.com',
+        image: '../../assets/images/oldresume.png',
+        description: 'Along with the Dependable Builders website, I also made my resume website to market myself going into my last semester of ' +
+        'college (Fall \'15) and applying for jobs to start my career.'
+      }
+    ]
   }
 })();
